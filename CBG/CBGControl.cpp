251dfd54560c1e9f694a5a3f5564fedc984f80ea
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-09-29 15:16:45 +0200

    Finished basic implementation. Finished luma and vectorial options implementation

--- CBG/CBGControl.cpp
+++ CBG/CBGControl.cpp
@@ -3,19 +3,98 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "CmdLineParser.hpp"
 #include <iostream>
+#include <cmath>
 
 using std::cout;
 using std::stof;
 using namespace cv;
 
+void updateContrast(int pos, void* userdata){
+    float *ptr = (float*)userdata;
+    *ptr = pos/100.0;
+};
 
+void updateGamma(int pos, void* userdata){
+    float *ptr = (float*)userdata;
+    *ptr = pos/100.0;
+};
+
+void updateBrightness(int pos, void* userdata){
+    float *ptr = (float*)userdata;
+    *ptr = (pos/100.0) - 1;
+};
+
+void updateBool(int state, void* ptr){
+    bool *val = (bool*) ptr;
+    *val = state;
+}
+
+void applyVec(const Mat &img, Mat &img2, const float c, const float g, const float b, const bool luma){
+    int nChannels = img.channels();
+    img2 = img.clone();
+    Mat channels[nChannels];
+    float *ptr[nChannels];
+    if((nChannels == 1) || (!luma)){
+        split(img2, channels);
+        for(int i = 0; i < nChannels; ++i){
+            ptr[i] = channels[i].ptr<float>(0);
+        }
+        for(int i = 0; i < nChannels; ++i){
+            cv::pow(channels[i], g, channels[i]);
+            multiply(channels[i], Scalar(c), channels[i]);
+            add(channels[i], Scalar(b), channels[i]);
+        }
+        merge(channels, nChannels, img2);
+    }
+    else{
+        cvtColor(img2, img2, CV_RGB2HSV);
+        split(img2, channels);
+        cv::pow(channels[2], g, channels[2]);
+        multiply(channels[2], Scalar(c), channels[2]);
+        add(channels[2], Scalar(b), channels[2]);
+        merge(channels, nChannels, img2);
+        cvtColor(img2, img2, CV_HSV2RGB);
+    }
+}
+
+void apply(const Mat &img, Mat &img2, const float c, const float g, const float b, const bool luma){
+    int nChannels = img.channels();
+    img2 = img.clone();
+    Mat channels[nChannels];
+    float *ptr[nChannels];
+    if((nChannels == 1) || (!luma)){
+        split(img2, channels);
+        for(int i = 0; i < nChannels; ++i){
+            ptr[i] = channels[i].ptr<float>(0);
+        }
+        for(int offset = 0; offset < img2.rows*img2.cols; ++offset){
+            for(int i = 0; i < nChannels; ++i){
+                *(ptr[i] + offset) = c*pow(*(ptr[i] + offset), g) + b;
+            }
+        }
+        merge(channels, nChannels, img2);
+    }
+    else{
+        cvtColor(img2, img2, CV_RGB2HSV);
+        split(img2, channels);
+        for(int i = 0; i < nChannels; ++i){
+            ptr[i] = channels[i].ptr<float>(0);
+        }
+        for(int offset = 0; offset < img2.rows*img2.cols; ++offset){
+            *(ptr[2] + offset) = c*pow(*(ptr[2] + offset), g) + b;
+        }
+        merge(channels, nChannels, img2);
+        cvtColor(img2, img2, CV_HSV2RGB);
+    }
+}
 
 
 int main(int argc, char** argv){
     float contrast = 1.0, gamma = 1.0, brightness = 0.0;
-    bool interactive = false;
 
-    Mat img, normalized, edited;
+    bool interactive = false, luma = false, vectorial = false;
+
+    Mat img, normalized, normEdited, edited;
     CmdLineParser commands(argc, argv);
     if(argc < 3){
         cout << "Usage: " << argv[0] <<" [options] <source> <output>\n";
@@ -25,25 +104,40 @@ int main(int argc, char** argv){
     if((commands["-g"]) && (stof(commands("-g")) >= 0) && (stof(commands("-c")) <= 2)) gamma = stof(commands("-g"));
     if((commands["-b"]) && (stof(commands("-b")) >= -1) && (stof(commands("-c")) <= 1)) brightness = stof(commands("-b"));
     if(commands["-i"]) interactive = true;
-
+    if(commands["-luma"]) luma = true;
+    if(commands["-vec"]) vectorial = true;
+    float oldContrast = contrast,  oldGamma = gamma, oldBrightness = brightness;
     img = imread(argv[argc-2]);
     namedWindow("Processed");
     if(img.rows == 0){
         cout << "Error reading image " << argv[argc-2] << '\n';
         exit(1);
     }
-
     imshow("Processed", img);
-
     if(interactive){
         int *c = new int, *g = new int, *b = new int;
         *c = (int)(100*contrast);
-        *g = 100*gamma;
-        *b = 100*brightness;
-        createTrackbar("Contrast", "Processed", c, 200, NULL, 0);
-        createTrackbar("Gamma", "Processed", g, 200, NULL, 0);
-        createTrackbar("Brightness", "Processed", b, 200, NULL, 0);
+        *g = (int)(100*gamma);
+        *b = (int)(100*(brightness+1));
+        createTrackbar("Contrast", "Processed", c, 200, updateContrast, (void*)&contrast);
+        createTrackbar("Gamma", "Processed", g, 200, updateGamma, (void*)&gamma);
+        createTrackbar("Brightness", "Processed", b, 200, updateBrightness, (void*)&brightness);
     }
-    while(waitKey(0) != 27);
+
+    img.convertTo(normalized, CV_32F, 1.0/255.0);
+    apply(normalized, normEdited, contrast, gamma, brightness, luma);
+    normEdited.convertTo(edited, img.type(), 255);
+    imshow("Processed", edited);
+
+    while(waitKey(50) != 27){
+        if((oldContrast != contrast) || (oldGamma != gamma) || (oldBrightness != brightness)){
+            vectorial?
+                applyVec(normalized, normEdited, contrast, gamma, brightness, luma)
+                :apply(normalized, normEdited, contrast, gamma, brightness, luma);
+            normEdited.convertTo(edited, img.type(), 255);
+            oldContrast = contrast,  oldGamma = gamma, oldBrightness = brightness;
+        }
+        imshow("Processed", edited);
+    }
     return 0;
 }
